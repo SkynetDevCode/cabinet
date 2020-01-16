de11a15de4915a3ff73147b0807c0ae900a2ad05
Virtual <skynetprogram42.appnet@gmail.com>
2020-01-16 22:20:20 +0100

    init
    init

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="container">
-        <span class="text-muted">&copy; Thibautl SAINTURET & Lucas OESLICK - Projet Cabinet Médical (IUT Informatique de Rangueil).</span>
+        <span class="text-muted">&copy; Régiodev</span>
     </div>
 </footer>
